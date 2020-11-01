2a2042c67fca69e21c1a5321f4b62fe512266761
Chikweze <cchikwez@ur.rochester.edu>
2020-11-01 16:25:07 -0500

    wait?

--- p.lisp
+++ p.lisp
@@ -1,4 +1,14 @@
 (defun append(a b)
     (cond ((car x)) (cons (car x) (append (cdr x) b))
 )
-hello
+
+
+(defun PROJECT
+    (princ "Welcome to my Project\n")
+    (princ "I completed all the assignments. 
+            Please do enter a number for the session you want to work on")
+    (setq section (read))
+    (princ section)
+)
+
+(PROJECT)
